ce73478d09835bcb34ba928edd38de6c2c155ea9
tarryTraveller <747525586@qq.com>
2020-10-15 11:21:01 +0800

    初构建

---
+++ yanze/src/test/java/com/yanze/building/BuildingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yanze.building;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
